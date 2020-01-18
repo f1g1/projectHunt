4c6be0d2da73a3994c90d00b2f8114ea8f961aca
unknown <filipoiugeorge98@gmai.com>
2020-01-18 16:27:29 +0200

    working login android

---
+++ firebase.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBqc4G-r6iQmC0RBEq6XfcCDJKmGMj6_Cw",
+    authDomain: "projecthunt-3268c.firebaseapp.com",
+    databaseURL: "https://projecthunt-3268c.firebaseio.com",
+    projectId: "projecthunt-3268c",
+    storageBucket: "projecthunt-3268c.appspot.com",
+    messagingSenderId: "633870399945",
+    appId: "1:633870399945:web:1c0a42e8de96238e4f1018",
+    measurementId: "G-MR3N35GFZS"
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,79 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import { IonContent, IonText, IonRow, IonItem, IonThumbnail, IonLabel, IonCol, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonImg } from '@ionic/react';
+import React, { Component } from 'react';
+import './Login.css';
+import { Plugins } from '@capacitor/core';
+import "@codetrix-studio/capacitor-google-auth";
 
-const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Ionic Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
-      </IonContent>
-    </IonPage>
-  );
+const INITIAL_STATE = {
+  loggedIn: true,
+  user: {}
 };
+
+class Home extends Component {
+  state: any = {};
+  props: any = {};
+  constructor(props: any) {
+    super(props);
+    this.state = { ...INITIAL_STATE };
+  }
+
+  componentDidMount() {
+    this.getUserInfo();
+  }
+
+  async signOut(): Promise<void> {
+    const { history } = this.props;
+    await Plugins.GoogleAuth.signOut();
+    history.goBack();
+  }
+
+  async getUserInfo() {
+    this.setState({
+      user: {
+        name: this.props.location.state.name,
+        image: this.props.location.state.image,
+        email: this.props.location.state.email
+      }
+    })
+  }
+
+  render() {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar color="primary">
+            <IonTitle>Logged in ... </IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+
+          <IonRow>
+            <IonCol className="text-center">
+              <IonText className="title">
+                You are logged in !
+              </IonText>
+            </IonCol>
+          </IonRow>
+
+          {this.state.user.name &&
+            <IonItem>
+              <IonThumbnail slot="start">
+                <img src={this.state.user.image} />
+              </IonThumbnail>
+              <IonLabel>
+                <h3>{this.state.user.name}</h3>
+                <p>{this.state.user.email}</p>
+              </IonLabel>
+            </IonItem>
+          }
+
+          <IonButton className="login-button" onClick={() => this.signOut()} expand="full" fill="solid" color="danger">
+            Logout from Google
+        </IonButton>
+        </IonContent>
+      </IonPage>
+    )
+  }
+}
 
 export default Home;
