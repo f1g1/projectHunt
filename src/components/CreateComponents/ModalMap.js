b532cf8ca989dad094fe8d40d7f1698740b83fb1
unknown <filipoiugeorge98@gmai.com>
2020-01-31 16:55:09 +0200

    before startging work on the map

--- src/components/CreateComponents/ModalMap.js
+++ src/components/CreateComponents/ModalMap.js
@@ -1,36 +1,86 @@
-import React, { useState } from "react";
-import { IonHeader, IonContent, IonToolbar, IonTitle, IonButton, IonInput, IonRow, IonFooter } from '@ionic/react';
-import SimpleMap from "../Map/SimpleMap";
+import React, { useState, useEffect, useRef } from "react";
+import { IonHeader, IonContent, IonButton, IonInput, IonRow, IonFooter, IonCol } from '@ionic/react';
+import GoogleMapReact from 'google-map-react';
+// import "./Map/Map.css"
+
 
 import "./ModalMap.css"
-function ModalMap(props) {
-
-  const [value, setValue] = useState("");
+let ModalMap = (props) => {
+  const [actualCenter, setActualCenter] = useState()
   const [center, setCenter] = useState(props.defaultLocation !== undefined ? props.defaultLocation : { lat: 0, lng: 0 })
-  const getCenter = (center) => {
-    debugger
+  const [value, setValue] = useState("");
+
+
+  let refMap = useRef(null);
+
+
+  const handleGetCenter = () => {
+
+    setCenter(actualCenter);
   }
-  console.log(center);
+
+  const handleBoundsChanged = () => {
+  };
+
+  let defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+
+
+  useEffect(() => {
+    setCenter(defaultProps.center)
+  }, [])
 
   return <>
+
+
     <IonHeader>
       <IonRow>
-        <IonInput placeholder="lng" className="inpt" value={center.lat || 0} onChange={(e) => { if (e.target.value !== center.lng) center && setCenter({ ...center, lng: e.target.value }) }}> </IonInput>
-        <IonInput placeholder="lat:" className="inpt" value={center.lng || 0} onChange={(e) => { if (center && e.target.value !== center.lat) center && setCenter({ ...center, lat: e.target.value }) }}></IonInput>
+
+        <IonCol size="5">
+          <IonInput placeholder="lat:" value={center ? center.lat : ""} onChange={(e) => { if (center && e.target.value !== center.lat) center && setCenter({ ...center, lat: e.target.value }) }}></IonInput>
+
+        </IonCol>
+
+        <IonCol align-self-center>
+          /
+        </IonCol>
+        <IonCol size="5">
+          <IonInput placeholder="lng:" value={center ? center.lng : ""} onChange={(e) => { if (e.target.value !== center.lng) center && setCenter({ ...center, lng: e.target.value }) }}> </IonInput>
+        </IonCol>
 
       </IonRow>
     </IonHeader>
-    <IonContent >
+    <IonContent>
+      <div style={{ height: '100%', width: '100%' }}>
+        <GoogleMapReact
+          ref={refMap}
+          bootstrapURLKeys={{
+            key: "AIzaSyAzTj3HlinSmLhPkJWlV1Swo0rEO_MDvoU"
+          }}
+          onChange={x => console.log(x)}
+          defaultCenter={defaultProps.center}
+          defaultZoom={defaultProps.zoom}
+          onDragEnd={(map) => setActualCenter({ lat: map.getCenter().lat(), lng: map.getCenter().lng() })}
+        >
+          {center && <div lat={center.lat} lng={center.lng}>V</div>}
+        </GoogleMapReact>
 
-      <SimpleMap getCenter={getCenter} showCenter />
+        <div className="markerFixed">X</div>
+      </div>
       <div className="bottomContainer">
         <IonButton color="danger" onClick={props.handleClose} >
           X
         </IonButton >
-        <IonButton>SAVE POSITION</IonButton>
+        <IonButton onClick={handleGetCenter}>SAVE POSITION</IonButton>
         <IonInput value={value} onChange={e => setValue(e.target.value)}></IonInput>
       </div>
     </IonContent>
+
 
   </>
 }
--- src/components/Map/SimpleMap.js
+++ src/components/Map/SimpleMap.js
@@ -1,32 +1,19 @@
-import GoogleMapReact from 'google-map-react';
-import "./Map.css";
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+
 import React from 'react'
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-let SimpleMap = (props) => {
-
-  let defaultProps = {
-    center: {
-      lat: 59.95,
-      lng: 30.33
-    },
-    zoom: 11
-  };
-
-  return (
-    // Important! Always set the container height explicitly
-    <div style={{ height: '100vh', width: '100%' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{
-          key: "AIzaSyAzTj3HlinSmLhPkJWlV1Swo0rEO_MDvoU"
-        }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
-      >
-        <div className="marker">X</div>
-      </GoogleMapReact>
-    </div>
-  );
+function MapContainer(props) {
+    return (
+        <Map
+            google={props.google}
+            zoom={8}
+            initialCenter={{ lat: 47.444, lng: -122.176 }}
+        >
+        </Map>
+    )
 }
 
-export default SimpleMap;
+
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyAzTj3HlinSmLhPkJWlV1Swo0rEO_MDvoU'
+})(MapContainer);
